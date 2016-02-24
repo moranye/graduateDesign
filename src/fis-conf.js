d6c90b92843768b58206631c7026e14cf15fb9fc
guoshuli <guoshuli@baidu.com>
2016-02-24 17:16:19 +0800

    add modules regulation

--- src/fis-conf.js
+++ src/fis-conf.js
@@ -1,15 +1,110 @@
-fis.config.set('pack', {
-    '/js/core.js': [
-        'js/constellation.js',
-        'js/data.js',
-        'js/detailbox.js',
-        'js/fun.js',
-        'js/gif.js',
-        'js/hotbox.js',
-        'js/navigation.js',
-        'js/video.js'
-    ],
-    '/css/aio.css': '**.css'
+fis.config.merge({
+    modules: {
+        parser: {
+            less: 'less',
+            tmpl: 'utc'
+        },
+        postprocessor: {
+            js: "jswrapper, require-async",
+            html: "require-async"
+        },
+        postpackager : ['autoload', 'simple'],
+        lint : {
+            js : 'jshint'
+        }
+    },
+    roadmap: {
+        domain: 'http://uisharer.com/graduateDesign',
+        ext: {
+            less: 'css'
+        },
+        path : [
+            {
+                reg : /^\/modules\/([^\/]+)\/\1\.(js)$/i,
+                isMod : true,
+                id : '$1',
+                release : '$&'
+            },
+            {
+                reg : /^\/modules\/(.*)\.(js)$/i,
+                isMod : true,
+                id : '$1',
+                release : '$&'
+            },
+            {
+                reg: /_bak-.*\.css$/,
+                release: false
+            },
+            {
+                reg : /^(.*)\.(css|less)$/i,
+                useSprite : true,
+                release : '$&'
+            },
+            {
+                reg: /_bak-.*\.html$/,
+                release: false
+            },
+            {
+                reg: /\/template\/.*html$/,
+                release: false
+            },
+            {
+                reg: /^\/libs\/.*\.(js|swf)$/,
+                release: '/js/$&',
+                useHash: false
+            },
+            {
+                reg : /.*\.html/,
+                useCache : false,
+                release : '$&'
+            },
+            {
+                reg : 'README.md',
+                release : false
+            }
+        ]
+    },
+    settings: {
+        postprocessor: {
+            jswrapper: {
+                type: 'amd'
+            }
+        },
+        jshint : {
+            i18n : 'zh-CN',
+            camelcase : true,
+            curly : true,
+            eqeqeq : true,
+            forin : true,
+            immed : true,
+            latedef : true,
+            newcap : true,
+            noarg : true,
+            noempty : true,
+            node : true
+        }
+    },
+    pack: {
+        '/js/lib.js': [
+            '/libs/dialog.js',
+            '/libs/jquery-1.11.3.min.js'
+        ],
+
+        '/js/core.js': [
+            'js/data.js',
+            'js/detailbox.js',
+            'js/mainContent.js',
+            'js/fun.js',
+            'js/gif.js',
+            'js/hotbox.js',
+            'js/navigation.js',
+            'js/constellation.js',
+            'js/video.js'
+        ]},
+    deploy: {
+        dist: {
+            to: '../publish',
+            exclude: /^(?:\/modules\/|\/libs\/)/i
+        }
+    }
 });
-fis.config.set('modules.postpackager', 'simple');
-fis.config.set('settings.postpackager.simple.autoCombine', true);
--- src/js/mainContent.js
+++ src/js/mainContent.js
@@ -1,134 +1,291 @@
 /**
  * Created by guoshuli on 2016/1/19.
  */
-(function(){
-var searchBox ={
-    events:function(){
-        var that = this,
-            bdSearch = $('.bd-search'),
-            bdSearchWrapper = $('.bd-search-wrapper'),
-            bdSearchButton = $('.bd-search-button');
-        that.searchIndex = 0;
-        bdSearch.on('focus',function(){
-            bdSearchWrapper.addClass('bd-search-wrapper-blue')
-        });
-        bdSearch.on('blur',function(e){
-            setTimeout(function(){
-                bdSearchWrapper.removeClass('bd-search-wrapper-blue');
-                that.clearSearchResult();
-            },100)
+(function () {
+    var searchBox = {
+        events: function () {
+            var that = this,
+                bdSearch = $('.bd-search'),
+                bdSearchWrapper = $('.bd-search-wrapper'),
+                bdSearchButton = $('.bd-search-button');
+            that.searchIndex = 0;
+            bdSearch.on('focus', function () {
+                bdSearchWrapper.addClass('bd-search-wrapper-blue')
+            });
+            bdSearch.on('blur', function (e) {
+                setTimeout(function () {
+                    bdSearchWrapper.removeClass('bd-search-wrapper-blue');
+                    that.clearSearchResult();
+                }, 100)
 
 
-        });
-        bdSearch.on('keyup',function(e){
-            var bdS = $('.bd-search'),
-                val = bdS.val(),
-                text = '';
-            if(!val){
-                that.clearSearchResult();
-                return
-            }
-
-            if(e.keyCode == 13){
-                if(val){
-                    window.open('https://www.baidu.com/s?&ie=utf-8&wd='+val);
-                    return;
-                }
-                else{
+            });
+            bdSearch.on('keyup', function (e) {
+                var bdS = $('.bd-search'),
+                    val = bdS.val(),
+                    text = '';
+                if (!val) {
+                    that.clearSearchResult();
                     return
                 }
+
+                if (e.keyCode == 13) {
+                    if (val) {
+                        window.open('https://www.baidu.com/s?&ie=utf-8&wd=' + val);
+                        return;
+                    }
+                    else {
+                        return
+                    }
+                }
+                else if (e.keyCode == 40) {
+                    var searchList = $('.result-list-item');
+                    if (!searchList) {
+                        return;
+                    }
+
+                    if (that.searchIndex >= searchList.length) {
+                        that.searchIndex = 0;
+                    }
+                    searchList.eq(that.searchIndex).addClass('result-list-item-hover').siblings().removeClass('result-list-item-hover');
+                    text = searchList.eq(that.searchIndex).find('a').text();
+                    bdS.val(text);
+                    that.searchIndex++;
+
+                }
+                else if (e.keyCode == 38) {
+                    var searchList = $('.result-list-item');
+                    if (!searchList) {
+                        return;
+                    }
+                    --that.searchIndex;
+
+                    if (that.searchIndex < 0) {
+                        that.searchIndex = searchList.length - 1;
+                    }
+                    searchList.eq(that.searchIndex).addClass('result-list-item-hover').siblings().removeClass('result-list-item-hover');
+                    text = searchList.eq(that.searchIndex).find('a').text();
+                    bdS.val(text);
+                }
+                else {
+                    that.getSearchData(val);
+                }
+
+            });
+            bdSearchButton.on('click', function (e) {
+                var val = $('.bd-search').val();
+
+                if (!val) {
+                    return
+                }
+                window.open('https://www.baidu.com/s?&ie=utf-8&wd=' + val);
+            })
+        },
+
+        clearSearchResult: function () {
+            var searchResult = $('.search-result'),
+                that = this;
+            searchResult.hide();
+            searchResult.html('');
+            that.searchIndex = 0;
+
+        },
+
+        getSearchData: function (val) {
+            var that = this,
+                str = '',
+                resultListItems = $('<ul class="result-list-items">'),
+                searchResult = $('.search-result');
+            Data.getKeyWords(val).done(function (data) {
+                if (!data.s) {
+                    that.clearSearchResult();
+                    return
+                }
+                else {
+                    $.each(data['s'], function (i, item) {
+                        str = str + '<li class="result-list-item"><a href="https://www.baidu.com/s?&ie=utf-8&wd=' + item + '" target="_blank">' + item + '</a></li>'
+                    });
+                    resultListItems.html(str);
+                    searchResult.html(resultListItems);
+                    searchResult.show();
+                    that.clickA();
+                }
+            })
+        },
+
+        clickA: function () {
+            var a = $('.result-list-items'),
+                that = this;
+            a.on('click', 'a', function (e) {
+                e.preventDefault();
+                var url = $(this).attr('href');
+                //that.clearSearchResult();
+                window.open(url);
+            })
+        },
+
+        init: function () {
+            var that = this;
+            that.searchIndex = 0;
+            that.events();
+        }
+    };
+    var remoteControl = {
+
+        cardsShow: function () {
+            var sectionCardHandle = $('.section-card-handle');
+            sectionCardHandle.show();
+        },
+
+        cardsHide: function () {
+            var sectionCardHandle = $('.section-card-handle');
+            sectionCardHandle.hide();
+        },
+
+        setLocalStorage: function () {
+            var arr = [],
+                sectionCards = $('.section-card'),
+                length = sectionCards.length;
+            for (var i = 0; i < length; i++) {
+                if ($('.section-card').eq(i).css('display') == 'block') {
+                    arr[i] = {
+                        name: sectionCards.eq(i).attr('id'),
+                        show: true
+                    }
+                }
+                else {
+                    arr[i] = {
+                        name: sectionCards.eq(i).attr('id'),
+                        show: false
+                    }
+                }
+
             }
-            else if (e.keyCode == 40){
-                var searchList = $('.result-list-item');
-                if(!searchList){
-                    return;
-                }
-
-                if(that.searchIndex>=searchList.length){
-                    that.searchIndex = 0;
-                }
-                searchList.eq(that.searchIndex).addClass('result-list-item-hover').siblings().removeClass('result-list-item-hover');
-                text = searchList.eq(that.searchIndex).find('a').text();
-                bdS.val(text);
-                that.searchIndex++;
-
+            if(!$('.section-card:visible').length) {
+                $.each(arr,function(i,item){
+                    item['show'] = false;
+                })
             }
-            else if(e.keyCode == 38) {
-                var searchList = $('.result-list-item');
-                if(!searchList){
-                    return;
-                }
-                --that.searchIndex;
+            console.log(JSON.stringify(arr));
+            localStorage.setItem('cards', JSON.stringify(arr));
+        },
 
-                if(that.searchIndex<0){
-                    that.searchIndex = searchList.length-1;
+        events: function () {
+            var that = this;
+            var settingButton = $('.setting-button');
+            var sectionContainer = $('.section-container');
+            var sectionButtonClose = $('.section-button-close');
+            var sectionBottomButton = $('.section-bottom-button');
+            var sectionHardHandle = $('.section-card-handle');
+            var sectionButtonRemove = $('.section-button-remove');
+            var cardListItems = $('.card-list-items');
+            settingButton.on('click', function (e) {
+                sectionContainer.fadeIn();
+                that.cardsShow()
+            });
+
+            sectionButtonClose.on('click', function (e) {
+                sectionContainer.fadeOut();
+                that.cardsHide();
+            });
+
+            sectionBottomButton.on('click', function (e) {
+                sectionButtonClose.trigger('click');
+            });
+
+            sectionButtonRemove.on('click', function (e) {
+                var parent = $(this).parents('.section-card').eq(0);
+                var checked = $('.card-list-item-input:checked');
+                var visible = $('.section-button-remove:visible');
+                if(visible.length <= 1){
+                    checked.prop('disabled',true);
+                    visible.prop('disabled',true);
                 }
-                searchList.eq(that.searchIndex).addClass('result-list-item-hover').siblings().removeClass('result-list-item-hover');
-                text = searchList.eq(that.searchIndex).find('a').text();
-                bdS.val(text);
+                else {
+                    checked.prop('disabled',false);
+                    visible.prop('disabled',false);
+                }
+                parent.fadeOut();
+                that.setLocalStorage();
+                that.initButtonState();
+            });
+            cardListItems.on('change','input',function(e){
+                var id = $(this).data('id');
+                var checked = $('.card-list-item-input:checked');
+                var visible = $('.section-button-remove:visible');
+                if(checked.length <= 1){
+                    checked.prop('disabled',true);
+                    visible.prop('disabled',true);
+                }
+                else {
+                    checked.prop('disabled',false);
+                    visible.prop('disabled',false);
+                }
+
+                if($(this).prop('checked') == true) {
+                    $('.section-card[id ='+id+']').fadeIn();
+                        that.setLocalStorage();
+                }
+                else {
+                    $('.section-card[id ='+id+']').fadeOut();
+                        that.setLocalStorage();
+
+                }
+
+            })
+
+        },
+        initButtonState:function(){
+            var storage = JSON.parse(localStorage.getItem('cards'));
+            var cardListItems = $('.card-list-item input');
+            if(!storage) {
+                cardListItems.prop('checked',true);
             }
             else {
-                that.getSearchData(val);
-            }
-
-        });
-        bdSearchButton.on('click',function(e){
-            var val = $('.bd-search').val();
-
-            if(!val) {
-                return
-            }
-            window.open('https://www.baidu.com/s?&ie=utf-8&wd='+val);
-        })
-    },
-
-    clearSearchResult:function(){
-        var searchResult = $('.search-result'),
-            that = this;
-        searchResult.hide();
-        searchResult.html('');
-        that.searchIndex = 0;
-
-    },
-    
-    getSearchData: function (val) {
-        var that = this,
-            str = '',
-            resultListItems = $('<ul class="result-list-items">'),
-            searchResult = $('.search-result');
-        Data.getKeyWords(val).done(function(data){
-            if(!data.s) {
-                that.clearSearchResult();
-                return
-            }
-            else {
-                $.each(data['s'],function(i,item){
-                    str = str+'<li class="result-list-item"><a href="https://www.baidu.com/s?&ie=utf-8&wd='+item+'" target="_blank">'+item+'</a></li>'
+                $.each(storage,function(i,item) {
+                    if(item['show'] == true) {
+                        $('.card-list-item-input[data-id='+item['name']+']').prop('checked',true);
+                    }
+                    else {
+                        $('.card-list-item-input[data-id='+item['name']+']').prop('checked',false);
+                    }
                 });
-                resultListItems.html(str);
-                searchResult.html(resultListItems);
-                searchResult.show();
-                that.clickA();
             }
-        })
-    },
+        },
 
-    clickA:function(){
-        var a = $('.result-list-items'),
-            that = this;
-        a.on('click','a',function(e){
-            e.preventDefault();
-            var url = $(this).attr('href');
-        //that.clearSearchResult();
-            window.open(url);
-        })
-    },
+        initModules: function (obj) {
+            if(!obj){
+                return
+            }
+            $.each(obj,function(i,item) {
+                if(item['show'] == true){
+                    $('.section-card[id='+item['name']+']').show();
+                }
+                else {
+                    $('.section-card[id='+item['name']+']').hide();
+                }
+            });
+        },
 
-    init:function(){
-        var that = this;
-        that.searchIndex = 0;
-        that.events();
-    }
-};
+        render: function () {
+            var that = this;
+            var tempObj = JSON.parse(localStorage.getItem('cards'));
+            if(!tempObj) {
+                that.initButtonState();
+                that.events();
+            }
+            else{
+                that.initModules(tempObj);
+                that.initButtonState();
+                that.events();
+            }
+        },
+
+        init: function () {
+            var that = this;
+            that.render();
+        }
+    };
     searchBox.init();
+    remoteControl.init();
 }())
